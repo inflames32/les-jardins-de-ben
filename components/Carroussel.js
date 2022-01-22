0dce75e0abe179e99b4c2dcbad32c3089018c542
unknown <cahuzac.p@gmail.com>
2022-01-22 01:18:42 +0100

    carousel WIP

---
+++ components/Carroussel.js
@@ -0,0 +1,5 @@
+import styles from "../styles/Carroussel.module.scss";
+
+const Carroussel = () => {
+  return <div className={styles.carroussel}>CARROUSEL</div>;
+};
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
+import { Carroussel } from "../components/Carroussel";
 import { Presentation } from "../components/Presentation";
 import styles from "../styles/Home.module.scss";
 import realisationJson from "../realisations.json";
@@ -10,6 +11,7 @@ import { FaAngleUp } from "react-icons";
 
 export const Home = () => {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
+  const [CarrousselIsOpen, setCarrousselIsOpen] = useState(false);
 
   const handleBurgerMenu = () => {
     if (menuIsOpen) {
@@ -19,6 +21,14 @@ export const Home = () => {
       console.log("je ferme le menu");
       setMenuIsOpen(true);
     }
+  };
+
+  const handleCarroussel = (name) => {
+    console.log("je lance le carroussel");
+    setCarrousselIsOpen(true);
+  };
+  const closeCarroussel = () => {
+    setCarrousselIsOpen(false);
   };
 
   return (
@@ -39,12 +49,61 @@ export const Home = () => {
         <section id="mesrealisations">
           <div className={styles.grid}>
             <h2 className={styles.h2}>Mes réalisations</h2>
+            {CarrousselIsOpen && (
+              <div className={styles.carousel}>
+                <div className={styles.backward}>
+                  <Image
+                    src="/27323.svg"
+                    alt="image précédente"
+                    className={styles.images}
+                    width={120}
+                    height={80}
+                  />
+                </div>
+                <div key="key" className="">
+                  {realisationJson.map((image, index) => {
+                    return (
+                      <Image
+                        name={image.description}
+                        key={image.key}
+                        src={image.image}
+                        alt="image précédente"
+                        className={styles.images}
+                        width={120}
+                        height={80}
+                      />
+                    );
+                  })}
+                </div>
+                <div className={styles.forward}>
+                  <Image
+                    src="/27324.svg"
+                    alt="image suivante"
+                    className={styles.images}
+                    width={120}
+                    height={80}
+                  />
+                </div>
+                <div
+                  className={styles.close_carroussel}
+                  onClick={closeCarroussel}
+                >
+                  <Image
+                    src="/32px-Black_close_x.svg"
+                    width="30"
+                    height="30"
+                    alt="close_button"
+                  />
+                </div>
+              </div>
+            )}
             <ul className={styles.ul}>
-              {realisationJson.map((elem, key) => (
-                <li className={styles.elem} key={elem.description}>
+              {realisationJson.map((elem) => (
+                <li className={styles.elem} key={elem.key}>
                   <article className={styles.card}>
                     <Image
-                      key={key}
+                      onClick={handleCarroussel}
+                      key={elem.key}
                       src={elem.image}
                       alt={elem.image}
                       className={styles.images}
@@ -58,18 +117,25 @@ export const Home = () => {
             </ul>
           </div>
         </section>
-        <section id="mespartenaires">Mes partenaires</section>
-        <section>Contact</section>
+        <section id="mespartenaires">
+          <title>
+            <h2>Mes partenaires</h2>
+          </title>
+        </section>
+        <section>
+          <title>
+            <h2>Contact</h2>
+          </title>
+        </section>
       </main>
       <div className={styles.scrollUp}>
         <a href="#accueil">
-          {" "}
           <Image
             src="/angle-up-svgrepo-com.svg"
             width={40}
             height={40}
             alt="FaAngleUp"
-            className={styles.svg}
+            className={styles.scrollUp_svg}
           />
         </a>
       </div>
