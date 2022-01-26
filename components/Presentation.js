9db3875e9bccb472e008523b823529e52ca50285
unknown <cahuzac.p@gmail.com>
2022-01-26 12:07:22 +0100

    description text ok

--- components/Presentation.js
+++ components/Presentation.js
@@ -13,13 +13,32 @@ export const Presentation = () => {
           height={350}
         />
       </div>
+
       <div className={styles.description}>
         <h2 className={styles.h2}>Qui est Ben ? </h2>
-        <p className={styles.descriptionText}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero unde
-          sequi hic corrupti obcaecati voluptates adipisci cupiditate placeat
-          reiciendis, eos a ducimus veniam numquam quaerat
-        </p>
+        <div className={styles.text}>
+          <div className={styles.guillemet}>
+            <Image
+              src="/images/ouvrir-guillemet.png"
+              alt="image 1"
+              width={60}
+              height={60}
+            />
+          </div>
+          <p className={styles.descriptionText}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero unde
+            sequi hic corrupti obcaecati voluptates adipisci cupiditate placeat
+            reiciendis, eos a ducimus veniam numquam quaerat
+          </p>
+          <div className={styles.guillemet}>
+            <Image
+              src="/images/fermer-guillemet.png"
+              alt="image 1"
+              width={60}
+              height={60}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
